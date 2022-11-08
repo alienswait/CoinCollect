cd3b9881098f8dfae2c9cace7bfb615904574b4f
Mert can Ünek <mrtcnunk@gmail.com>
2022-11-08 20:39:04 +0300

    update

---
+++ Coin/Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float MovementSpeed;
+    [SerializeField] float HorSpeed;
+    float Hor;
+
+    void Update()
+    {
+        Hor = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(Hor * HorSpeed * Time.deltaTime, 0, MovementSpeed * Time.deltaTime));
+    }
+}
