ed4d34275f9fb4df81cefd6b7ee6306647dc6f84
Mert can Ünek <mrtcnunk@gmail.com>
2022-11-09 00:20:58 +0300

    update

---
+++ Coin/Assets/Scripts/AxeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeController : MonoBehaviour
+{
+    public GameObject target;
+    void Update()
+    {
+        
+        transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
